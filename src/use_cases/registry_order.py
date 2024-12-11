0cdaad70c325df9471a5e0d43412f3ca89711a86
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-10 22:08:37 -0300

    feat: Implemented validation use on registry_oerders

--- src/use_cases/registry_order.py
+++ src/use_cases/registry_order.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from src.models.repository.interfaces.orders_repository import OrdersRepositoryInterface
 from src.main.http_types.http_request import HttpRequest
 from src.main.http_types.http_response import HttpResponse
+from src.validators.registry_order_validator import registry_order_validator
 
 
 class RegistryOrder:
@@ -9,10 +10,22 @@ class RegistryOrder:
         self.__orders_repository = orders_repository
 
     def registry(self, http_request: HttpRequest) -> HttpResponse:
-        body = http_request.body
-        new_order = self.__format_new_order(body)
-        self.__registry_order(new_order)
-        return self.__format_response()
+        try:
+            body = http_request.body
+            self.__validate_body(body)
+
+            new_order = self.__format_new_order(body)
+            self.__registry_order(new_order)
+
+            return self.__format_response()
+        except Exception as exception:
+            return HttpResponse(
+                body={"error": str(exception)},
+                status_code=400
+            )
+
+    def __validate_body(self, body: dict):
+        registry_order_validator(body)
 
     def __format_new_order(self, body: dict) -> dict:
         new_order = body["data"]
